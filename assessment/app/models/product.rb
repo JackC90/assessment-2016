ed5fef6198cd537d97ddf4310d5514f7b21c99f8
Chua Jack Yune <jckynchua@yahoo.com>
2016-12-23 10:46:12 +0800

    Added enumeration for product language. Added discount column for products.

--- assessment/app/models/product.rb
+++ assessment/app/models/product.rb
@@ -9,15 +9,16 @@ class Product < ApplicationRecord
 	enum category: [:biology, :chemistry, :physics, :economics, :finance, :history, :literary_fiction, :fantasy, :scifi, :horror]
 	enum sale_or_rent: [:for_sale, :for_rent]
 	enum format: [:paperback, :hardback, :other]
+	enum language: [:English, :Malay, :Chinese]
 
 	# Scopes for Searching
 	pg_search_scope :search_string, :against => [:title, :description], using: {tsearch: {dictionary: "english"}}
 	pg_search_scope :search_isbn, :against => [:isbn]
-	scope :category, 		-> (category) { where(category: Product.categories[category]) }
-	scope :sale_or_rent, 	-> (sale_or_rent) { where(sale_or_rent: Product.sale_or_rents[sale_or_rent]) }
+	scope :category, 		-> (category) { where(category: category) }
+	scope :sale_or_rent, 	-> (sale_or_rent) { where(sale_or_rent: sale_or_rent) }
 	scope :ages, 			-> (ages) { where("ages >= ?", ages.to_i) }
 	scope :price_above, 	-> (price) { where("price >= ?", price) }
 	scope :price_below, 	-> (price) { where("price <= ?", price) }
-	scope :format, 			-> (format) { where(format: Product.formats[format])}
+	scope :format, 			-> (format) { where(format: format)}
 	scope :language, 		-> (language) { where(language: language)}
 end
